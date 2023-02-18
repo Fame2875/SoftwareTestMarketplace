67cdfffcad0d4451f46e7d697a18a6b271d1e431
chanasorn chungmarisakul <69136207+Fame2875@users.noreply.github.com>
2023-02-18 16:00:29 +0700

    Update

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,19 +1,31 @@
-import { Col, Row } from "react-bootstrap"
-import Itemcard from "../components/Itemcard"
+import { useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Itemcard from '../components/Itemcard';
+
 export function Category() {
-    return (
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleCardMouseEnter = () => {
+    setSelectedItem(null);
+  };
+
+  return (
     <>
-    <div className="justify-content-center align-items-center d-flex mt-2">
-    <img style={{ height: '400px', width: 'auto' }} srcSet="/mockimgs/PTO.png" alt="Store Banner" />
-    </div>
+      <div className="justify-content-center align-items-center d-flex mt-2">
+        <img style={{ height: '400px', width: 'auto' }} srcSet="/mockimgs/PTO.png" alt="Store Banner" />
+      </div>
       <h1>Category</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
-        <Itemcard/>
+        <Itemcard onClick={handleItemClick} onMouseEnter={handleCardMouseEnter} isSelected={selectedItem?.id === 1} />
+        <Itemcard onClick={handleItemClick} onMouseEnter={handleCardMouseEnter} isSelected={selectedItem?.id === 2} />
+        {/* Add more Itemcard components here */}
       </Row>
     </>
-
-    )
-
-
+  );
 }
+
 export default Category;
--- src/components/Itemcard.jsx
+++ src/components/Itemcard.jsx
@@ -1,47 +1,68 @@
-import React from 'react'
-import { Button, Card } from "react-bootstrap"
+import React, { useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Itemcard() {
-return (
-  <>
-    <Card className="h-100" >
-      <Card.Img
-        variant="top"
-        height="200px"
-        src= "mockimgs/PTO.png"
-        style={{ objectFit: "cover" }}
-      />
-      <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-        <div class="row">
-          <div>
-          <span className="fs-2">test</span>
-          </div>
-          <div>
-          <span className="ms-2 text-muted">หลายบาท</span>
-          </div>
-        </div>
-        </Card.Title>
-        <div className="mt-auto">
-          
-            <Button className=" w-100" style={{ background : "LimeGreen"}}>
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleClick = () => {
+    // Handle click event here
+  };
+
+  return (
+    <>
+      <Card
+        className="h-100"
+        style={{
+          boxShadow: isHovered ? '0px 0px 10px 5px gray' : 'none',
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
+      >
+        <Card.Img
+          variant="top"
+          height="200px"
+          src="mockimgs/PTO.png"
+          style={{ objectFit: 'cover' }}
+        />
+        <Card.Body className="d-flex flex-column">
+          <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+            <div class="row">
+              <div>
+                <span className="fs-2">test</span>
+              </div>
+              <div>
+                <span className="ms-2 text-muted">หลายบาท</span>
+              </div>
+            </div>
+          </Card.Title>
+          <div className="mt-auto">
+            <Button className="w-100" style={{ background: 'LimeGreen' }}>
               + ใส่ตะกร้า
-            </Button> 
+            </Button>
             <div
               className="d-flex align-items-center flex-column"
-              style={{ gap: ".5rem" }}
+              style={{ gap: '.5rem' }}
             >
               <div
                 className="d-flex align-items-center justify-content-center"
-                style={{ gap: ".5rem" }}
-              >
-                
-              </div>
+                style={{ gap: '.5rem' }}
+              ></div>
             </div>
-        </div>
-      </Card.Body>
-    </Card>
-  </>
-  )
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
-  export default Itemcard;
+
+export default Itemcard;
